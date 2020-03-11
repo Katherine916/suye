c8709dfc7d5ee565aba5a7403cd9c0153d3abbaa
Katherine916 <1182875980@qq.com>
2020-03-11 23:27:41 +0800

    苏叶2318

---
+++ five/amusement_park.py
@@ -0,0 +1,21 @@
+age = 12
+if age < 4:
+    print("You admission cost is $0")
+elif age <18:
+    print("You need to cost is $5")
+else:
+    print("YOu admission cost is $10")
+
+
+
+
+
+
+age = 19
+if age < 4:
+    price = 0
+elif age <18:
+    price = 5
+else:
+    price = 10
+print("You admission cost is $"+str(price))
