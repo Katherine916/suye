dfafbc42b6affafb8904a4b2789536e669901c58
Katherine916 <1182875980@qq.com>
2020-03-22 22:33:03 +0800

    seven

---
+++ seven/greeter.py
@@ -0,0 +1,12 @@
+name = input("Please enter your name:")
+print("Hello,"+name+"!")
+
+prompt = "If you tell us who you are,we can personalize the messages you see."
+prompt = "\nWhat is yout first name?"
+name = input(prompt)
+
+print("\nHello,"+name+"!")
+
+age = input("How old are you?")
+
+print("年龄是："+str(int(age)))
---
+++ seven/test71.py
@@ -0,0 +1,6 @@
+car = input("What you want car?")
+
+print("Let me see if I can find you a Subaru "+car)
+
+people = input("How many people"
+               "")
