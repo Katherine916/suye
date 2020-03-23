2335375afeb3035499b64005523571f033d4929c
Katherine916 <1182875980@qq.com>
2020-03-23 23:03:29 +0800

    seven7.2.3

--- seven/parrot.py
+++ seven/parrot.py
@@ -1,2 +1,10 @@
-message = input("Tell me something,and I will repeat it back to you:")
-print(message)
+message1 = input("Tell me something,and I will repeat it back to you:")
+print(message1)
+
+promot = "\n Tell me something, and I will repeat it back to you:"
+promot = "\n Enter 'quit' to end the program."
+message = ""
+while message != "quit":
+    message = input(promot)
+    if message != "quit":
+        print(message)
--- seven/test71.py
+++ seven/test71.py
@@ -2,5 +2,17 @@ car = input("What you want car?")
 
 print("Let me see if I can find you a Subaru "+car)
 
-people = input("How many people"
-               "")
+people = input("How many people? ")
+people = int(people)
+if people > 8:
+    print("No desk")
+else:
+    print("Please come in ")
+
+number = input("Please input a number:")
+number = int(number)
+
+if number%10 == 0:
+    print("数字 "+str(number)+" 是10的倍数")
+else:
+    print("数字 " + str(number) + " 不是10的倍数")
