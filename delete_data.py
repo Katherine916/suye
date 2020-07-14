7738641c678b9158f5cacb1c0d7ac73fd2eec50a
eme <songruimin@xforceplus.com>
2020-07-14 14:51:49 +0800

    删除数据脚本

--- delete_data.py
+++ delete_data.py
@@ -5,55 +5,59 @@ import requests
 import json
 from urllib import parse
 
-check_user = parse.quote('[{"field":"create_user_name","op":"eq","values":"世茂生产测试账号"}]')
+check_user = parse.quote('[{"field":"create_user_name","op":"eq","values":"益海嘉里测试账号FAT"}]')
 
+#无条件删除
+# check_user = parse.quote('[]')
 
-# 查询的sit接口
-check_url = "http://web.pscc.shimaogroup.com:8080/data/{}?pageNo=1&pageSize=999999&params="+check_user
-# 删除的sit接口
-delete_url = 'http://web.pscc.shimaogroup.com:8080/data/{}'
+# 查询/删除的FAT接口
+check_url = "http://elephant-image-fat.phoenix-t.xforceplus.com/data/{}?pageNo=1&pageSize=9999&params=" + check_user
+delete_url = 'http://elephant-image-fat.phoenix-t.xforceplus.com/data/{}'
 
+# 查询/删除的SIT接口
+# check_url = "http://elephant-image-sit.phoenix-t.xforceplus.com/data/{}?pageNo=1&pageSize=9999&params=" + check_user
+# delete_url = 'http://elephant-image-sit.phoenix-t.xforceplus.com/data/{}'
 
+# #查询/删除的UAT接口
+# check_url = "http://web.pscc-uat.shimaogroup.com/data/{}?pageNo=1&pageSize=9999&params="+check_user
+# delete_url = 'http://web.pscc-uat.shimaogroup.com//data/{}'
 
-allapi_dict = {
-
-    'label': 'label',                                            #标签
-    'image': 'image',                                            #影像
-    'ticket':'ticket',                                           #单证
-    'configDataMapping':'configDataMapping',                     #识别规则
-    'configDataMappingItem':'configDataMappingItem',             #识别规则细则
-    'ticketInvoice':'ticketInvoice',                             #增值税发票
-    'ticketTrain':'ticketTrain',
-    'ticketMany':'ticketMany',
-    'ticketAttachment':'ticketAttachment',
-    'ticketTaxi':'ticketTaxi'
-}
+#益海嘉里
+# check_url = "https://fsscscpuat-image.wilmar.cn/data/{}?pageNo=1&pageSize=9999&params="+ check_user
+# delete_url = 'https://fsscscpuat-image.wilmar.cn/data/{}'
 
 
 api_dict = {
-
-    'image': 'image'                                       #影像
-    # 'ticket':'ticket',                                       #单证
-    # 'ticketInvoice':'ticketInvoice',                         #增值税发票
-    # 'ticketTrain':'ticketTrain',
-    # 'ticketMany':'ticketMany',
-    # 'ticketAttachment':'ticketAttachment',
-    # 'ticketTaxi':'ticketTaxi',
-    #'baseBill':'baseBill',
-    #'salesBill':'salesBill'
+    'label': 'label',
+    'image': 'image',  # 影像
+    'ticket': 'ticket',  # 单证
+    'ticketInvoice': 'ticketInvoice',  # 增值税发票
+    'ticketTrain': 'ticketTrain',
+    'ticketMany': 'ticketMany',
+    'ticketAttachment': 'ticketAttachment',
+    'ticketTaxi': 'ticketTaxi',
+    'baseBill': 'baseBill',
+    'salesBill': 'salesBill',
+    'billTicketRelation':'billTicketRelation',
+    'billTicketRelation':'billTicketRelation',          #单据单证复用关系表
+    'ticketSourceFile':'ticketSourceFile',               #电票源文件
+    'ticketInvoiceDetail':'ticketInvoiceDetail',        #增值税发票明细
+    'ticketPlaneDetail':'ticketPlaneDetail',            #飞机票明细
+    'ticketMachineDetail':'ticketMachineDetail',        #通用机打发票明细
 
 }
 
-
 checkdata = []
+
+
 def check_func(api_type):
     new_check_url = check_url.format(api_dict.get(api_type))
     response = requests.get(new_check_url)
     # 查询数据
     if response.status_code == 200:
-    #获取到查询结果的数据json解码后赋值给 result
+        # 获取到查询结果的数据json解码后赋值给 result
         result = json.loads(response.text)
-    #获取到字典中key为 list的值
+        # 获取到字典中key为 list的值
         lists = result["list"]
     for list in lists:
         mylist = list["id"]
@@ -62,8 +66,7 @@ def check_func(api_type):
 
 
 # 删除查询到的列表数据
-def delete_func(delete_list,api_type):
-
+def delete_func(delete_list, api_type):
     new_delete_url = delete_url.format(api_dict.get(api_type))
     for data in delete_list:
         requests.delete(url=new_delete_url, params={"ids[]": data})
@@ -71,11 +74,6 @@ def delete_func(delete_list,api_type):
 
 if __name__ == '__main__':
 
-    i=400;
-    while i>0:
-     for myname in api_dict:
-
-      delete_func(check_func(myname),myname)
-
-      i=i-1;
-      print(str(i)+"删除结束！！！！！！！")
+    for myname in api_dict:
+        delete_func(check_func(myname), myname)
+print("删除结束！！！！！！！")
