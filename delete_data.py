1b75fb3dde4c3735d21641d92c66dc5615e7a841
eme <songruimin@xforceplus.com>
2020-07-14 14:55:25 +0800

    清除删除脚本的网址

--- delete_data.py
+++ delete_data.py
@@ -5,28 +5,6 @@ import requests
 import json
 from urllib import parse
 
-check_user = parse.quote('[{"field":"create_user_name","op":"eq","values":"益海嘉里测试账号FAT"}]')
-
-#无条件删除
-# check_user = parse.quote('[]')
-
-# 查询/删除的FAT接口
-check_url = "http://elephant-image-fat.phoenix-t.xforceplus.com/data/{}?pageNo=1&pageSize=9999&params=" + check_user
-delete_url = 'http://elephant-image-fat.phoenix-t.xforceplus.com/data/{}'
-
-# 查询/删除的SIT接口
-# check_url = "http://elephant-image-sit.phoenix-t.xforceplus.com/data/{}?pageNo=1&pageSize=9999&params=" + check_user
-# delete_url = 'http://elephant-image-sit.phoenix-t.xforceplus.com/data/{}'
-
-# #查询/删除的UAT接口
-# check_url = "http://web.pscc-uat.shimaogroup.com/data/{}?pageNo=1&pageSize=9999&params="+check_user
-# delete_url = 'http://web.pscc-uat.shimaogroup.com//data/{}'
-
-#益海嘉里
-# check_url = "https://fsscscpuat-image.wilmar.cn/data/{}?pageNo=1&pageSize=9999&params="+ check_user
-# delete_url = 'https://fsscscpuat-image.wilmar.cn/data/{}'
-
-
 
 
 checkdata = []
